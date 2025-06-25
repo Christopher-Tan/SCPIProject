d63a4bcd6beddbf6a282ae8ed5fb6430a9193c63
Christopher Tan <72955765+Christopher-Tan@users.noreply.github.com>
2025-06-25 09:31:23 +0200

    GUI improvements

--- GUI.py
+++ GUI.py
@@ -1,18 +1,31 @@
 ip = "146.136.39.137"
 port = 50024
 
-from OST import CouplingMeasurer
-
-instrument = CouplingMeasurer(f"TCPIP::{ip}::{port}::SOCKET", read_termination="\n", write_termination="\n")
-
 import streamlit as st
 from streamlit_extras.grid import grid
+from streamlit_navigation_bar import st_navbar
 
 st.set_page_config(page_title="Coupling Measurements", layout="wide")
 
-g = grid([2, 8, .5, 1.5], vertical_align='bottom')
+
+
+from OST import CouplingMeasurer
+instrument = CouplingMeasurer(f"TCPIP::{ip}::{port}::SOCKET", read_termination="\n", write_termination="\n")
+g = grid([2, 8, 3, 3], vertical_align='center')
 
 g.empty()
 g.title("Coupling Measurements")
-g.empty()
-g.button("Measure", on_click=instrument.measure, args=(), key="measure_button")
+if g.button("Measure", args=(), key="measure_button"):
+    try:
+        instrument.measure()
+    except Exception as e:
+        print(e)
+        st.error(f"Failed to connect to and perform a measurement on the instrument")
+
+if g.button("Reset", args=(), key="reset_button"):
+    try:
+        instrument.reset()
+    except Exception as e:
+        print(e)
+        st.error(f"Failed to connect to and reset the instrument")
+
