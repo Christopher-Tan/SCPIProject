089843e86e5e60f07051409cc9324f4b53ce62d6
Christopher Tan <72955765+Christopher-Tan@users.noreply.github.com>
2025-07-17 19:31:13 +0200

    Changes

--- source/GUI.py
+++ source/GUI.py
@@ -21,6 +21,36 @@ if len(sys.argv) > 1 and sys.argv[1] == "streamlit":
     
     from math import log10, floor, isclose
     
+    def cleanup():
+        try:
+            instrument.adapter.close()
+        except:
+            pass
+    
+    from streamlit.runtime.scriptrunner import add_script_run_ctx
+    from streamlit.runtime.scriptrunner_utils.script_run_context import get_script_run_ctx
+    
+    from streamlit.runtime import get_instance
+    
+    import threading
+    
+    def heartbeat(user):
+        thread = threading.Timer(interval=2, function=heartbeat, args=(user,)) # prepare to run in new thread, this current thread will be killed then
+        add_script_run_ctx(thread) # inject current main thread context into the to-be-started new thread
+    
+        ctx = get_script_run_ctx() # fetch own context variables, explicitly for analysis
+        
+        runtime = get_instance() # main runtime with all instances, including myself
+        
+        if runtime.is_active_session(session_id=ctx.session_id):
+            thread.start() # actually start the new thread to keep this heartbeat going
+        else:
+            return cleanup()
+    
+    if "heartbeat" not in st.session_state:
+        heartbeat(get_script_run_ctx().session_id)
+        st.session_state["heartbeat"] = True
+        
     if not config['developerMode']:
         st.markdown("""
             <style>
@@ -107,16 +137,19 @@ if len(sys.argv) > 1 and sys.argv[1] == "streamlit":
             st.session_state['frequency'], st.session_state['frequency_units'], st.session_state['frequency_scaling'] = replace_prefix(instrument.frequency, properties['freq']['units'])
             st.session_state['nPrim'], st.session_state['nPrim_units'] = replace_suffix(instrument.nPrim, properties['nPrim']['units'])
             st.session_state['nSec'], st.session_state['nSec_units'] = replace_suffix(instrument.nSec, properties['nSec']['units'])
-            st.session_state['history'] = int(instrument.n)
-            st.session_state['max_history'] = int(instrument.n)
+            nn = int(instrument.n)
+            if nn != st.session_state['max_history']:
+                st.session_state['history'] = nn
+                st.session_state['max_history'] = nn
             
     from OST import CouplingMeasurer
     if 'instrument' not in st.session_state:
         try:
-            instrument = CouplingMeasurer(f"TCPIP::{ip}::{port}::SOCKET", read_termination="\n", write_termination="\n", timeout=10000)
+            with st.spinner("Connecting...", show_time=True):
+                instrument = CouplingMeasurer(f"TCPIP::{ip}::{port}::SOCKET", read_termination="\n", write_termination="\n", timeout=10000)
             st.session_state['instrument'] = instrument
             fetch()
-            #st.success(f"Connected to {instrument.id}.")
+            st.toast(f"Connected to {instrument.id}.")
         except Exception as e:
             instrument = None
             #st.error(f"Failed to connect to the instrument at {ip}:{port}. Please check the connection and try again.")
@@ -128,7 +161,8 @@ if len(sys.argv) > 1 and sys.argv[1] == "streamlit":
     g.title("Coupling Measurements")
     if g.button("Measure", args=(), key="measure_button"):
         try:
-            instrument.measure()
+            with st.spinner("Waiting for measurement to complete...", show_time=True):
+                instrument.measure()
             fetch()
         except Exception as e:
             print(e)
@@ -146,6 +180,8 @@ if len(sys.argv) > 1 and sys.argv[1] == "streamlit":
             
     if "refresh_before" not in st.session_state:
         st.session_state["refresh_before"] = False
+        
+    if "refresh_after" not in st.session_state:
         st.session_state["refresh_after"] = False
         
     def refresh():
@@ -157,9 +193,9 @@ if len(sys.argv) > 1 and sys.argv[1] == "streamlit":
         
     g = grid([0.86, 0.6, 0.86, 0.6, 0.86, 0.6, 0.86, 0.6], vertical_align='bottom')
     try:
-        instrument.voltage = g.number_input("Voltage", key="voltage", format="%0.3f", on_change=refresh, step=1.0/st.session_state['voltage_scaling'], min_value=properties['voltLvl']['min'], max_value=properties['voltLvl']['max']) * st.session_state['voltage_scaling']
+        instrument.voltage = g.number_input("Voltage", key="voltage", format="%0.3f", on_change=refresh, step=1.0/st.session_state['voltage_scaling'], min_value=properties['voltLvl']['min']/st.session_state['voltage_scaling'], max_value=properties['voltLvl']['max']/st.session_state['voltage_scaling']) * st.session_state['voltage_scaling']
         g.write(st.session_state['voltage_units'])
-        instrument.frequency = g.number_input("Frequency", key="frequency", format="%0.3f", on_change=refresh, step=1.0/st.session_state['frequency_scaling'], min_value=properties['freq']['min'], max_value=properties['freq']['max']) * st.session_state['frequency_scaling']
+        instrument.frequency = g.number_input("Frequency", key="frequency", format="%0.3f", on_change=refresh, step=1.0/st.session_state['frequency_scaling'], min_value=properties['freq']['min']/st.session_state['frequency_scaling'], max_value=properties['freq']['max']/st.session_state['frequency_scaling']) * st.session_state['frequency_scaling']
         g.write(st.session_state['frequency_units'])
         instrument.nPrim = g.number_input("nPrim", key="nPrim", on_change=refresh, step=1, min_value=properties['nPrim']['min'], max_value=properties['nPrim']['max'])
         g.write(st.session_state['nPrim_units'])
@@ -176,9 +212,9 @@ if len(sys.argv) > 1 and sys.argv[1] == "streamlit":
         #st.error(f"Failed to connect to and set the instrument parameters")
         
     if "n" not in st.session_state:
-        st.session_state["n"] = None
+        st.session_state["n"] = "Raw Data"
 
-    n = st_navbar(["Raw Data", "T-Model", "Gamma-Model"], selected=None, adjust=False, styles={
+    n = st_navbar(["Raw Data", "T-Model", "Gamma-Model"], selected=st.session_state["n"], adjust=False, styles={
             'nav': {
                 'background-color': 'rgba(0, 0, 0, 0)',
                 'margin': '0px',
@@ -207,8 +243,9 @@ if len(sys.argv) > 1 and sys.argv[1] == "streamlit":
         }
     )
     
-    if n:
+    if n and n != st.session_state["n"]:
         st.session_state["n"] = n
+        st.rerun()
 
     n = st.session_state["n"]
     
@@ -249,7 +286,7 @@ if len(sys.argv) > 1 and sys.argv[1] == "streamlit":
             
     def gamma_model(data):
         with schemdraw.Drawing(show=False) as d:
-            t = elm.xform.Transformer().label(f'{1} : {data["N"]:.4f}', fontsize=10)
+            t = elm.xform.Transformer().label(f'{1} : {data["N"]}' if data["N"] else ":", fontsize=10)
             
             p1 = elm.Line().up().at(t.p1).length(0.5)
             p2 = elm.Line().down().at(t.p2).length(0.5)
@@ -273,6 +310,11 @@ if len(sys.argv) > 1 and sys.argv[1] == "streamlit":
         def format_with_units(value, units):
             value, units, _ = replace_prefix(value, units)
             return f"{value:.3f} {units}"
+        
+        def special_format(value):
+            if isinstance(value, str):
+                return value
+            return f"{value:.3f}"
 
         if n == "Raw Data":
             data = {
@@ -280,13 +322,13 @@ if len(sys.argv) > 1 and sys.argv[1] == "streamlit":
                 'frequency': format_with_units(instrument.channels[st.session_state['history']].frequency, properties['freq']['units']),
                 'L1': format_with_units(instrument.channels[st.session_state['history']].L1, properties['L1']['units']),
                 'L2': format_with_units(instrument.channels[st.session_state['history']].L2, properties['L2']['units']),
-                'k': f"{instrument.channels[st.session_state['history']].k:.3f}",
-                'k1': f"{instrument.channels[st.session_state['history']].k1:.3f}",
-                'k2': f"{instrument.channels[st.session_state['history']].k2:.3f}",
+                'k': special_format(instrument.channels[st.session_state['history']].k),
+                'k1': special_format(instrument.channels[st.session_state['history']].k1),
+                'k2': special_format(instrument.channels[st.session_state['history']].k2),
                 'v1_prim': format_with_units(instrument.channels[st.session_state['history']].v1_prim, properties['v1_prim']['units']),
                 'v2_prim': format_with_units(instrument.channels[st.session_state['history']].v2_prim, properties['v2_prim']['units']),
                 'v1_sec': format_with_units(instrument.channels[st.session_state['history']].v1_sec, properties['v1_sec']['units']),
-                'v2_sec': format_with_units(instrument.channels[st.session_state['history']].v2_sec, properties['v2_sec']['units']),
+                'v2_sec': format_with_units(instrument.channels[st.session_state['should behistory']].v2_sec, properties['v2_sec']['units']),
             }
             st.table(data)
         elif n == "T-Model":
@@ -302,14 +344,17 @@ if len(sys.argv) > 1 and sys.argv[1] == "streamlit":
             data = {
                 'Ls': format_with_units(instrument.channels[st.session_state['history']].Ls, properties['Ls']['units']),
                 'Lp': format_with_units(instrument.channels[st.session_state['history']].Lp, properties['Lp']['units']),
-                'N': instrument.channels[st.session_state['history']].N,
+                'N': special_format(instrument.channels[st.session_state['history']].N),
             }
             gamma_model(data)
     except Exception as e:
         print(e)
         
     if st.session_state['max_history'] > 1:
-        st.slider("Measurement", min_value=1, max_value=st.session_state['max_history'], key="history")
+        v = st.slider("Measurement", min_value=1, max_value=st.session_state['max_history'], value=st.session_state['history'])
+        if v != st.session_state['history']:
+            st.session_state['history'] = v
+            st.rerun()
 
 elif __name__ == "__main__":
     import subprocess
